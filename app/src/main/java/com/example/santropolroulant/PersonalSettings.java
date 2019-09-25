4e787c311a9f568c601eadb637959eab0cf0805b
Jerome <jerome.marfleet@mail.mcgill.ca>
2019-09-24 20:05:50 -0400

    Writing to database. Only writing firstname, lastname, email, and phone so far.

--- app/src/main/java/com/example/santropolroulant/PersonalSettings.java
+++ app/src/main/java/com/example/santropolroulant/PersonalSettings.java
@@ -1,38 +1,37 @@
 package com.example.santropolroulant;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONException;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.santropolroulant.DataValueTypes.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
-import org.json.JSONObject;
-
-import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.Executor;
 
 import static android.R.style.Theme_Holo_Light_Dialog_MinWidth;
 
@@ -40,9 +39,11 @@ public class PersonalSettings extends AppCompatActivity {
     final Calendar myCalendar = Calendar.getInstance();
     EditText prefInputFirstname, prefInputLastname, prefInputDOB,
             prefInputEmail, prefInputPhone;
+    Button saveButton;
     DatePickerDialog.OnDateSetListener date;
     List<User> users;
     User myUser;
+    String firstname, lastname, dob, email, phone;
 
     private FirebaseAuth firebaseAuth;
     private DatabaseReference mDatabase;
@@ -60,7 +61,6 @@ public class PersonalSettings extends AppCompatActivity {
         setContentView(R.layout.activity_personal_settings);
 
 
-
         firebaseAuth = FirebaseAuth.getInstance();
         final FirebaseUser user = firebaseAuth.getCurrentUser();
         // Auto login for signed in user - Commented out below
@@ -74,33 +74,33 @@ public class PersonalSettings extends AppCompatActivity {
         prefInputDOB = findViewById(R.id.prefinput_birthday_dob);
         prefInputEmail = findViewById(R.id.prefinput_contact_email);
         prefInputPhone= findViewById(R.id.prefinput_contact_phone);
+        saveButton = findViewById(R.id.ps_save);
 
-        users = new ArrayList<User>();
-        mDatabase = FirebaseDatabase.getInstance().getReference("userSample");
-
-
-        mDatabase.addValueEventListener(new ValueEventListener() {
+        ValueEventListener saveChangesListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot userDS : dataSnapshot.getChildren()){
+                for (DataSnapshot userDS : dataSnapshot.getChildren()) {
                     User userCurr = userDS.getValue(User.class);
                     users.add(userCurr);
                 }
 
                 Log.i("User Email", user.getEmail());
 
-                for(User userTemp : users){
+                for (User userTemp : users) {
                     Log.i("Current Email", userTemp.getEmail());
-                    if (userTemp.getEmail().equals(user.getEmail())){
+                    if (userTemp.getEmail().equals(user.getEmail())) {
                         myUser = userTemp;
                     }
                 }
 
-                if(myUser != null){
-                    prefInputFirstname.setHint(myUser.getFirst_name());
-                    prefInputLastname.setHint(myUser.getLast_name());
-                    prefInputEmail.setHint(myUser.getEmail());
-                    prefInputPhone.setHint(myUser.getPhone_number());
+                if (myUser != null) {
+                    setHintStrings(
+                            myUser.getFirst_name(),
+                            myUser.getLast_name(),
+                            myUser.getEmail(),
+                            myUser.getPhone_number());
+                    setHints();
+
                 } else {
                     Log.e("User Selection", "User not Found");
                 }
@@ -110,10 +110,14 @@ public class PersonalSettings extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
-        });
+        };
+
+        users = new ArrayList<User>();
+        mDatabase = FirebaseDatabase.getInstance().getReference("userSample");
 
 
-        //prefInputFirstname.setHint();
+        mDatabase.addValueEventListener(saveChangesListener);
+
 
         date = new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -137,25 +141,78 @@ public class PersonalSettings extends AppCompatActivity {
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean editFirstname, editLastname, editEmail, editPhone, editSettings;
+                String inFirstname, inLastname, inEmail, inPhone;
+
+                editFirstname = editLastname = editEmail = editPhone = editSettings = false;
+                inFirstname = prefInputFirstname.getText().toString();
+                inLastname = prefInputLastname.getText().toString();
+                inEmail = prefInputEmail.getText().toString();
+                inPhone = prefInputPhone.getText().toString();
+
+                String key = myUser.getKey();
+
+                ArrayList<Task> tasks = new ArrayList<Task>();
+
+                //Check if user put any info into fields
+                if(!inFirstname.equals("")){ editFirstname = true; }
+
+                if(!inLastname.equals("")){ editLastname = true; }
+
+                if(!inEmail.equals("")){ editEmail = true; }
+
+                if(!inPhone.equals("")){ editPhone = true; }
+
+                //if so, upload new info
+                if(editFirstname || editLastname || editEmail || editPhone){
+                    if(editFirstname){
+                        tasks.add(mDatabase.child(key).child("first_name").setValue(inFirstname));
+                    }
+
+                    if(editLastname){
+                        tasks.add(mDatabase.child(key).child("first_name").setValue(inLastname));
+                    }
+
+                    if(editEmail){
+                        tasks.add(mDatabase.child(key).child("email").setValue(inEmail));
+                    }
+
+                    if(editPhone){
+                        tasks.add(mDatabase.child(key).child("phone").setValue(inPhone));
+                    }
+
+                    prefInputFirstname.getText().clear();
+                    prefInputLastname.getText().clear();
+                    prefInputEmail.getText().clear();
+                    prefInputPhone.getText().clear();
+
+                    Toast.makeText(PersonalSettings.this, "Changes have been saved!", Toast.LENGTH_SHORT).show();
+                } else { //if not, prompt user for input
+                    Toast.makeText(PersonalSettings.this, "Please Change Something!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 
-    /*private enum Type {
-        TELEPHONE,
-        EMAIL,
-
+    private void setHintStrings(String firstname, String lastname, String email, String phone){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.phone = phone;
     }
 
-    private class Preference<E>{
-
-
+    private void setHints(){
+        prefInputFirstname.setHint(myUser.getFirst_name());
+        prefInputLastname.setHint(myUser.getLast_name());
+        prefInputEmail.setHint(myUser.getEmail());
+        prefInputPhone.setHint(myUser.getPhone_number());
     }
 
-    private class PreferenceGroup{
-        public String title;
-        public Preference[] preferences;
-        PreferenceGroup(String title, Preference[] preferences){
-            this.title = title;
-            this.preferences = preferences;
-        }
-    }*/
 }
+
