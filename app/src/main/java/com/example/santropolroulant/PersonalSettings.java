e05668ef4c18252f864da2b59d109592d57c0188
BradMcBain <bradmcbaineng@gmail.com>
2019-09-26 17:02:51 -0400

    adding date picker

--- app/src/main/java/com/example/santropolroulant/PersonalSettings.java
+++ app/src/main/java/com/example/santropolroulant/PersonalSettings.java
@@ -1,16 +1,28 @@
 package com.example.santropolroulant;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class PersonalSettings extends AppCompatActivity {
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private TextView userBirthDate;
 
     //private PreferenceGroup[] settings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_settings);
+        setupUIViews();
     }
 
     /*private enum Type {
@@ -32,4 +44,33 @@ public class PersonalSettings extends AppCompatActivity {
             this.preferences = preferences;
         }
     }*/
+    private void setupUIViews(){
+        userBirthDate = (TextView) findViewById(R.id.prefinput_birthday_dob);
+        userBirthDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        PersonalSettings.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = month + "/" + day + "/" + year;
+                userBirthDate.setText(date);
+            }
+        };
+    }
 }
