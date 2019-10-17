a7214071a1ee75877b41e45be06abc992a554e1f
BradMcBain <bradmcbaineng@gmail.com>
2019-10-17 19:39:00 -0400

    Added map functionality, using same api key as firebase's

--- app/src/main/java/com/example/santropolroulant/ContactUs.java
+++ app/src/main/java/com/example/santropolroulant/ContactUs.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentActivity;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -17,20 +18,28 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class ContactUs extends AppCompatActivity implements OnMapReadyCallback {
+
+public class ContactUs extends FragmentActivity implements OnMapReadyCallback {
 
     private TextView title, phone, email, address;
     private GoogleMap mMap;
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+        setupUIViews();
+    }
+    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us); // Designates which layout XML to be used for this page
         setupUIViews();
-    }
+    }*/
 
 
     private void setupUIViews() {
@@ -51,13 +60,25 @@ public class ContactUs extends AppCompatActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng roulantHQ = new LatLng(45.516815, -73.575003);
+        mMap.addMarker(new MarkerOptions().position(roulantHQ).title("Santropol Roulant HQ"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(roulantHQ));
+        moveToCurrentLocation(roulantHQ);
     }
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    private void moveToCurrentLocation(LatLng currentLocation)
+    {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,15));
+        // Zoom in, animating the camera.
+        mMap.animateCamera(CameraUpdateFactory.zoomIn());
+        // Zoom out to zoom level 10, animating with a duration of 2 seconds.
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+
 
     }
 }
