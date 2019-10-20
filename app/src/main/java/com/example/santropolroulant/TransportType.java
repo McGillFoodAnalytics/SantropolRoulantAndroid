88329b8b32e670aa10c01484bedf08844972a148
Janna Agustin <jannalouisea@gmail.com>
2019-10-20 15:19:27 -0400

    fixed design for event registration classes

--- app/src/main/java/com/example/santropolroulant/TransportType.java
+++ app/src/main/java/com/example/santropolroulant/TransportType.java
@@ -19,7 +19,7 @@ public class TransportType extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private TextView tvTitle2;
-    private CardView crdDriver, crdBiker, crdWalker, crdPubTransport;
+    private CardView crdDriver, crdNonDriver;
     private Button btnNext2;
     String transportType;
 
@@ -38,24 +38,10 @@ public class TransportType extends AppCompatActivity {
             }
         });
 
-        crdBiker.setOnClickListener(new View.OnClickListener() {
+        crdNonDriver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 transportType = "deliv";
-            }
-        });
-
-        crdWalker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                transportType = "delis";
-            }
-        });
-
-        crdPubTransport.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                transportType = "delds";
             }
         });
 
@@ -78,10 +64,7 @@ public class TransportType extends AppCompatActivity {
         btnNext2 = (Button) findViewById(R.id.btnNext2);
 
         crdDriver = (CardView) findViewById(R.id.crdDriver);
-        crdBiker = (CardView) findViewById(R.id.crdBiker);
-        crdWalker = (CardView) findViewById(R.id.crdWalker);
-        crdPubTransport = (CardView) findViewById(R.id.crdPubTransport);
-
+        crdNonDriver = (CardView) findViewById(R.id.crdNonDriver);
 
     }
 
