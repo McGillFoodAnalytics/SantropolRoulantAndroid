076e042d23d3011a9a341db6ab18c8ea86d2be5c
BradMcBain <bradmcbaineng@gmail.com>
2020-01-11 22:31:38 -0500

    remade home page

--- app/src/main/java/com/example/santropolroulant/MainActivity.java
+++ app/src/main/java/com/example/santropolroulant/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,6 +18,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import android.os.CountDownTimer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private Button signupButton;
 
     private FirebaseAuth firebaseAuth;
+    private TextView timerView;
 
     public static final String EVENT_LOC = "eventSample";
     public static final String USER_LOC = "userSample";
@@ -35,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         // Setting up UI
         loginButton = (Button) findViewById(R.id.Login_Button);
         signupButton = (Button) findViewById(R.id.Create_Account_Button);
+        timerView = (TextView) findViewById(R.id.timerView);
 
         // Auto login for signed in user - Commented out below
         /*firebaseAuth = FirebaseAuth.getInstance();
@@ -61,5 +65,38 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, CreateAccount.class));
             }
         });
+
+        CountUpTimer timer = new CountUpTimer(100000000) {
+            public void onTick(int second) {
+                timerView.setText(String.valueOf(second));
+            }
+        };
+
+        timer.start();
+    }
+
+
+
+    public abstract class CountUpTimer extends CountDownTimer {
+        private static final long INTERVAL_MS = 500;
+        private final long duration;
+
+        protected CountUpTimer(long durationMs) {
+            super(durationMs, INTERVAL_MS);
+            this.duration = durationMs;
+        }
+
+        public abstract void onTick(int second);
+
+        @Override
+        public void onTick(long msUntilFinished) {
+            int second = (int) ((duration - msUntilFinished) / 500);
+            onTick(second);
+        }
+
+        @Override
+        public void onFinish() {
+            onTick(duration / 500);
+        }
     }
 }
