cab0740a4c8cd35f1aa7e1beaeb193a223e3cb0e
Jerome <jerome.marfleet@mail.mcgill.ca>
2020-01-22 14:52:03 -0400

    Consolidating two event classes

--- app/src/main/java/com/example/santropolroulant/PersonalSettings.java
+++ app/src/main/java/com/example/santropolroulant/PersonalSettings.java
@@ -16,10 +16,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.santropolroulant.DataValueTypes.Event;
 import com.example.santropolroulant.DataValueTypes.User;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
