f413565c161d766396a631746f75aa63767befd3
Jerome <jerome.marfleet@mail.mcgill.ca>
2020-01-22 16:39:11 -0400

    Fixed PS seeing changes when none were made.

--- app/src/main/java/com/example/santropolroulant/Home.java
+++ app/src/main/java/com/example/santropolroulant/Home.java
@@ -6,25 +6,18 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewTreeObserver;
-import android.widget.Toast;
 import android.widget.TextView;
 
-import com.example.santropolroulant.DataValueTypes.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 
