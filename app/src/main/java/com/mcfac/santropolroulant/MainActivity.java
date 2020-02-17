a5c3ca163e77270c2180a7947ea26b68788204d9
BradMcBain <bradmcbaineng@gmail.com>
2020-02-16 21:37:40 -0500

    adding translation button to main page

--- app/src/main/java/com/mcfac/santropolroulant/MainActivity.java
+++ app/src/main/java/com/mcfac/santropolroulant/MainActivity.java
@@ -1,10 +1,16 @@
 package com.mcfac.santropolroulant;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -22,6 +28,10 @@ import com.mcfac.santropolroulant.R;
 
 import android.os.CountDownTimer;
 
+import static android.content.DialogInterface.*;
+
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button loginButton;
@@ -35,7 +45,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadLocale();
         setContentView(R.layout.activity_main_page); //Designates which layout XML to be used for this page
+
+        //ActionBar actionBar = getSupportActionBar();
+
+       // actionBar.setTitle(getResources().getString(R.string.app_name));
+
 
         // Setting up UI
         loginButton = (Button) findViewById(R.id.Login_Button);
@@ -75,8 +91,84 @@ public class MainActivity extends AppCompatActivity {
         };
 
         timer.start();
+
+        Button changeLang = (Button) findViewById(R.id.changeMyLang);
+
+        changeLang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showChangeLanguageDialog();
+            }
+
+        });
+    }
+    private void showChangeLanguageDialog() {
+
+        final String[] listItems = {"French", "English"};
+
+        AlertDialog.Builder mbuilder = new AlertDialog.Builder(MainActivity.this);
+
+        mbuilder.setTitle("Choose Language...");
+
+        mbuilder.setSingleChoiceItems(listItems, -1, new OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+
+                if (i == 0) {
+
+                    setLocale("en");
+
+                    recreate();
+
+                } else if (i == 1) {
+
+                    setLocale("fr");
+
+                    recreate();
+
+                }
+
+                dialog.dismiss();
+
+            }
+
+        });
+
+        AlertDialog mDialog = mbuilder.create();
+
+        mDialog.show();
+
     }
 
+    private void setLocale(String lang) {
+
+        Locale locale = new Locale(lang);
+
+        Locale.setDefault(locale);
+
+        Configuration config = new Configuration();
+
+        config.locale = locale;
+
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+
+        SharedPreferences.Editor editor = getSharedPreferences( "Settings", MODE_PRIVATE).edit();
+
+        editor.putString("My_Lang", lang);
+
+        editor.apply();
+
+    }
+
+    public void loadLocale() {
+
+        SharedPreferences prefs = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+
+        String language = prefs.getString("My_Lang", "");
+
+        setLocale(language);
+    }
     @Override
     public void onBackPressed() {
         return;
